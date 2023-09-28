d4d0246f0e43eb868ff6325aa4ccbea97b7d60ed
Lucas Ribeiro <lucas_9ribeiro@hotmail.com>
2023-09-28 00:04:12 -0300

    adicionado variavel de ambiente

--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+require("dotenv/config")
 require("express-async-errors");
 
 const express = require('express');
@@ -28,7 +29,7 @@ app.use((error,request,response,next)=>{
 
 })
 
-const PORT = 3333
+const PORT = process.env.PORT || 3333
 
 
 app.listen(PORT,()=>{
